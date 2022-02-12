385e28fe25a36a133c6a4d1a9a5fee027212c1c7
Abhisek <abhisek159753@gmail.com>
2022-02-12 10:39:40 -0500

    Starting Rock paper scissors

---
+++ script.js
@@ -0,0 +1,13 @@
+function playRound(playerSelection, computerSelection){
+    //code stuff
+}
+
+function game(){
+    for (let i = 0; i< 5; i++){
+        playRound();
+        //code more stuff
+    }
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
